c4bfd9e1fb3e40f27707c82ca8b1860926a283c0
Armed Fish <yufante@vip.qq.com>
2020-02-07 10:04:02 +0800

    修改错误样式

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,28 +8,40 @@
         <h5>注册</h5>
       </div>
       <div class="card-body">
-        @include('shared._errors')
         <form method="POST" action="{{ route('users.store') }}">
             @csrf
+
             <div class="form-group">
               <label for="user_name">姓名：</label>
-              <input type="text" name="user_name" class="form-control" value="{{ old('user_name') }}">
+              <input type="text" name="user_name" class="@error('user_name') is-invalid @enderror form-control" value="{{ old('user_name') }}">
             </div>
+            @error('user_name')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
   
             <div class="form-group">
               <label for="user_account">账号：</label>
-              <input type="text" name="user_account" class="form-control" value="{{ old('user_account') }}">
+              <input type="text" name="user_account" class="@error('user_account') is-invalid @enderror form-control" value="{{ old('user_account') }}">
             </div>
+            @error('user_account')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
   
             <div class="form-group">
               <label for="password">密码：</label>
-              <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+              <input type="password" name="password" class="@error('password') is-invalid @enderror form-control" value="{{ old('password') }}">
             </div>
+            @error('password')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
   
             <div class="form-group">
               <label for="password_confirmation">确认密码：</label>
-              <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+              <input type="password" name="password_confirmation" class="@error('password_confirmation') is-invalid @enderror form-control" value="{{ old('password_confirmation') }}">
             </div>
+            @error('password_confirmation')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
   
             <button type="submit" class="btn btn-primary">注册</button>
         </form>
